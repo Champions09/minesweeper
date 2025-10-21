7721a91398b77868f135a1fbf43fb47e616a28ef
rishabhbalaji2008-ops <rishabhbalaji2008@gmail.com>
2025-10-21 14:34:18 +0530

    added a way to remove buttons and reveal what's underneath

--- number_placement.py
+++ number_placement.py
@@ -1,34 +1,3 @@
-'''import tkinter as tk
-import random
-
-a=tk.Tk()
-f=0 
-a.geometry("612x657")
-for r in range(9):
-    for c in range(9):
-        x=random.randint(0,100)
-        if x==0:
-            label=tk.Label(a,text="",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="black")
-        if x>=0 and x<=15 and f<10:
-            label=tk.Label(a,text="💣",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="white")
-            f+=1    
-        if x>=16 and x<=30:
-            label=tk.Label(a,text="",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="black")
-        if x>=31 and x<=50:
-            label=tk.Label(a,text="",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="black")
-        if x>51 :
-            label=tk.Label(a,text="",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="black")
-        label.grid(row=r, column=c, padx=0.5, pady=0.5)
-        button.grid(row=r, column=c, padx=0.5, pady=0.5)
-
-a.mainloop()
-'''
-
 import tkinter as tk
 import random
 
@@ -46,20 +15,27 @@ while len(mines) < total_mines:
     if n not in mines:
         mines.append(n)
 
-print(mines)
+current_grid_number = 1 
 
-current_grid_number = 1  
+l=[]
 
 for r in range(rows):
+    rows_buttons = []
     for c in range(cols):
         if current_grid_number in mines:
-            text = '💣'
-            bgc='grey'
+            label=tk.Label(a,text="💣",width=8,height=4)
+            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="white")
+            l.append(button)
         else:
-            text=''
-            bgc='white'
-
-        btn = tk.Button(a, text=text,width=8,height=4,relief="raised",bg=bgc)
-        btn.grid(row= r, column=c)
+            label=tk.Label(a,text="",width=8,height=4)
+            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="grey")
+            l.append(button)
+        label.grid(row=r, column=c, padx=0.5, pady=0.5)
+        button.grid(row=r, column=c, padx=0.5, pady=0.5)
         current_grid_number+=1
+    
+for i in l:
+    def on_click(b=i):
+        b.destroy()
+    i.config(command=on_click)
 a.mainloop()
