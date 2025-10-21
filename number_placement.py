7000f99bf103fe1799e641f3a9132571b4c8e941
Champions09 <jojobizzare0901@gmail.com>
2025-10-21 16:26:52 +0530

    added numbers to nearby the mines

--- number_placement.py
+++ number_placement.py
@@ -14,26 +14,61 @@ while len(mines) < total_mines:
     n = random.randint(1, total_cells)
     if n not in mines:
         mines.append(n)
+mines.sort()
+print(mines)
 
-current_grid_number = 1 
+def convert_num_to_grid(num):
+    row = (num - 1) // cols
+    col = (num - 1) % cols
+    return row, col
+
+grid =[]
+for r in range(rows):
+    grid.append([])
+    for c in range(cols):
+        grid[r].append(0)
+
+'''
+(r-1,c-1)  (r-1,c)  (r-1,c+1)
+(r,  c-1)  (r,  c)  (r,  c+1)
+(r+1,c-1)  (r+1,c)  (r+1,c+1)
+'''
+for num in mines:
+    r,c = convert_num_to_grid(num)
+    grid[r][c] = -1 #mine is present at this position
+    
+    #adding plus one to the neighbouring boxes
+    l=[r-1,r,r+1]
+    m=[c-1,c,c+1]
+    
+    for i in l:
+        for j in m:
+            if 0 <= i < rows and 0 <= j < cols and grid[i][j] != -1:
+                grid[i][j] += 1
 
 l=[]
-
 for r in range(rows):
-    rows_buttons = []
     for c in range(cols):
-        if current_grid_number in mines:
-            label=tk.Label(a,text="💣",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="white")
-            l.append(button)
+        val = grid[r][c]
+        if val == -1:
+            text = "💣"
+            bgc='red'
+        elif val == 0:
+            text = "0"
+            bgc='white'
         else:
-            label=tk.Label(a,text="",width=8,height=4)
-            button=tk.Button(a,text="",width=8,height=4,relief="raised",bg="grey")
-            l.append(button)
+            text = str(val)
+            bgc='white'
+
+        label = tk.Label(a, text=text, width=8, height=4, bg=bgc,relief='solid')
         label.grid(row=r, column=c, padx=0.5, pady=0.5)
+
+        button = tk.Button(a, text="", width=8, height=4, relief="raised", bg="grey")
         button.grid(row=r, column=c, padx=0.5, pady=0.5)
-        current_grid_number+=1
-    
+        
+        l.append(button)
+        
+
 for i in l:
     def on_click(b=i):
         b.destroy()
